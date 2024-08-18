ad3ec2bab5bbd3d6c6c1135b28bf0f0c1eae3be7
Ironious4 <tyronekariuki@gmail.com>
2024-08-18 18:27:06 +0300

    Update

--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -18,7 +18,7 @@ function BotsPage() {
   const handleBotDelete = async (botToDelete) => {
     try {
       // send delete request to the backend
-      await fetch(`http://localhost:3000/bots/${botToDelete.id}`, {
+      await fetch(`https://bots-challenge-amber.vercel.app/bots/${botToDelete.id}`, {
         method: "DELETE",
       });
       // Remove the bot from the army if it's in the army
